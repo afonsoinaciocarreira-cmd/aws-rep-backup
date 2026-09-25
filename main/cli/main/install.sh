129e7d151736ea9235047053738303b952bb8514
afonsoinaciocarreira-cmd <afonso.inacio.carreira@gmail.com>
2026-09-25 20:58:30 +0000

    Delete main/cli/main/install.sh

--- main/cli/main/install.sh
+++
@@ -1,8 +0,0 @@
-read -p "Inserir teste: " testeinput
-
-if [ -d "$testeinput" ]; then
-    echo "Aviso: A pasta '$testeinput' já existe!"
-else
-    mkdir "$testeinput"
-    echo "Sucesso: Dir '$testeinput'"
-fi
